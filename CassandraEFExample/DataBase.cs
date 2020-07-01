11a1f7d32979b0a51526f9a2dbbaf3b73352a361
Vladislav Sabenin <datdefboi@gmail.com>
2020-07-01 23:45:08 +0300

    Initial commit

---
+++ CassandraEFExample/DataBase.cs
@@ -0,0 +1,22 @@
+﻿using CassandraEFExample.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CassandraEFExample
+{
+    public class DataBase:DbContext
+    {
+        private DbSet<User> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseCassandra("Contact Points=127.0.0.1");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ForCassandraAddKeyspace("hl", new KeyspaceReplicationSimpleStrategyClass(2));
+            modelBuilder.Entity<User>().ToTable("Users", "hl").HasKey(p => p.Name);
+            modelBuilder.Entity<User>().ForCassandraSetClusterColumns(p => p.Name);
+        }
+    }
+}
